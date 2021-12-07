c1a15493de28a91cd6db25d18bf3e7c6f099c696
Caleb_Du <Caleb_Du@zju.edu.cn>
2021-12-07 13:09:07 +0800

    complete lc-1034 dfs/bfs

---
+++ lc-1034.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+using std::vector, std::pair;
+
+class Solution
+{
+    using PII = pair<int, int>;
+    vector<PII> direct = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    void dfs(vector<vector<int>> &grid, vector<vector<bool>> &vis, int i, int j, int origin, vector<PII> &boundary)
+    {
+        int m = grid.size(), n = grid[0].size();
+        bool isBoundary = false;
+        for(auto [x, y]: direct)
+        {
+            int nx = i+x, ny = j+y;
+            if(nx<0||nx>=m||ny<0||ny>=n||grid[nx][ny]!=origin)
+                isBoundary = true;
+            else if(!vis[nx][ny])
+            {
+                vis[nx][ny] = true;
+                dfs(grid, vis, nx, ny, origin, boundary);
+            }
+        }
+        if(isBoundary)
+            boundary.emplace_back(i, j);
+    }
+
+public:
+    vector<vector<int>> colorBorder(vector<vector<int>> &grid, int row, int col, int color)
+    {
+        int m = grid.size(), n = grid[0].size();
+        int origin = grid[row][col];
+        vector<vector<bool>> vis(m, vector<bool>(n, false));
+        vector<PII> boundary;
+        vis[row][col] = true;
+        dfs(grid, vis, row, col, origin, boundary);
+        for(auto [x, y]: boundary)
+            grid[x][y] = color;
+        return grid;
+    }
+};
+
+int main()
+{
+    Solution *sol = new Solution();
+    vector<vector<int>> grid = {{1, 2, 2}, {2, 3, 2}};
+    sol->colorBorder(grid, 0, 1, 3);
+}
